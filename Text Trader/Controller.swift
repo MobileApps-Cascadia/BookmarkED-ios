2a41228d8c5981240ffe0783be15a65d4db0bfd2
rjb888 <robert.j.bronson@gmail.com>
2022-04-22 14:38:04 -0700

    added controller file and placeholder functions for CRUD operations

---
+++ Text Trader/Controller.swift
@@ -0,0 +1,44 @@
+//
+//  Controller.swift
+//  Text Trader
+//
+//  Created by ROBERT BRONSON on 4/22/22.
+//  Copyright © 2022 Cascadia. All rights reserved.
+//
+
+import Foundation
+
+class Controller: ObservableObject{
+    
+    func addBook(){
+        
+    }
+    func registerUser(){
+        
+    }
+    func login(){
+        
+    }
+    func getUserInfo(){
+        
+    }
+    func getBook(){
+        
+    }
+    func getBooks(){
+        
+    }
+    func editBook(){
+        
+    }
+    func updateUserInfo(){
+        
+    }
+    func deleteBook(){
+        
+    }
+    func deleteUser(){
+        
+    }
+    
+}
